524cdd76317a9005762e5aa0512e655f82b3c364
Gira <bartunovaangira@mail.ru>
2026-06-01 00:05:41 +0300

    unify damage calculation into compute_damage

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass, field
 GRID_WIDTH = 10
 GRID_HEIGHT = 10
 PLAYER_MAX_HP = 10
-PLAYER_ATTACK_DAMAGE = 2
+PLAYER_ATTACK_DAMAGE = 3
 PLAYER_DEFENSE = 1
 MAX_PLAYERS = 4
 RNG_SEED = 42
@@ -17,6 +17,7 @@ class EnemyDef:
     name: str
     hp: int
     attack_damage: int
+    defense: int
     position: tuple[int, int]
 
 @dataclass
@@ -38,24 +39,28 @@ DEFAULT_LEVEL = LevelConfig(
             name="Goblin",
             hp=6,
             attack_damage=1,
+            defense=1,
             position=(3, 3),
         ),
         EnemyDef(
             name="Skeleton",
             hp=8,
             attack_damage=2,
+            defense=1,
             position=(6, 6),
         ),
         EnemyDef(
             name="Rat",
             hp=4,
             attack_damage=3,
+            defense=3,
             position=(7, 6),
         ),
         EnemyDef(
             name="Angry bunny",
             hp=7,
             attack_damage=0,
+            defense=2,
             position=(5, 3),
         )
 
